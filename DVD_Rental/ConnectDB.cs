4ca59a2ab1b19d1773ddd36c487559950032981b
YuushiSoma <yuushi.soma@gmail.com>
2020-10-19 16:08:09 +0900

    Ver. 1.0

---
+++ DVD_Rental/ConnectDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+
+namespace DVD_Rental
+{
+    public class ConnectDB
+    {
+        public string [] auth(string id , string pw)
+        {
+            String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=SquatDB;Integrated Security=True";
+            SqlConnection objConn = new SqlConnection(connectionString);
+            objConn.Open();
+
+            string sql = "select IsAdministrator from User where Id = " + id + " and LoginPassword = " + pw + "";
+
+            SqlCommand sqlCommand = objConn.CreateCommand();
+            sqlCommand.CommandText = sql;
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            sqlCommand.Dispose();
+            objConn.Close();
+
+            if (sqlDataReader.HasRows)
+            {
+                //IDとパスワードが一致した場合
+                string[] array = { sqlDataReader["IsAdminstrator"].ToString(), id };
+                return array;
+            }
+            else
+            {
+                //IDとパスワードの組み合わせがない場合
+                string[] array = {"-1", id};
+                return array;
+            }
+        }
+    }
+}
---
+++ DVD_Rental/Login.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DVD_Rental
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                Session["id_err_flag"] = 0;
+                Session["pw_err_flag"] = 0;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //ID : 15文字 英数字
+            //PW : 25文字 英数字
+
+            //user_idの英数字判定
+            if(new Regex("^[0-9a-zA-Z]+$").IsMatch(user_id.Text))
+            {
+                Session["id_err_flag"] = 0;
+
+                //passwdの英数字判定
+                if (new Regex("^[0-9a-zA-Z]+$").IsMatch(passwd.Text))
+                {
+                    //認証処理
+
+
+                    //データベース
+                    /*ConnectDB db = new ConnectDB();
+                    string[] status = db.auth(user_id.Text,passwd.Text);
+
+                    if(int.Parse(status[0]) == 1)
+                    {
+                        //管理者フラグ成立
+                        Session.Remove("id_err_flag");
+                        Session.Remove("pw_err_flag");
+                    }
+                    else if(int.Parse(status[0]) == 0)
+                    {
+                        //管理者フラグ非成立
+                        Session.Remove("id_err_flag");
+                        Session.Remove("pw_err_flag");
+                    }
+                    else
+                    {
+                        //ログインできない
+                    }*/
+                }
+                else
+                {
+                    //パスワードのエラー
+                    Session["pw_err_flag"] = 1;
+                }
+            }
+            else
+            {
+
+                //passwdの英数字判定
+                if (new Regex("^[0-9a-zA-Z]+$").IsMatch(passwd.Text))
+                {
+                    //IDのエラー
+                    Session["id_err_flag"] = 1;
+                }
+                else
+                {
+                    //IDとパスワードのエラー
+                    Session["id_err_flag"] = 1;
+                    Session["pw_err_flag"] = 1;
+                }
+            }
+        }
+    }
+}
