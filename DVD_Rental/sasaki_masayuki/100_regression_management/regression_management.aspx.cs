ccc6df58ef8840bf6593df2eb76c92cab3600d22
YuushiSoma <yuushi.soma@gmail.com>
2020-10-21 12:01:46 +0900

    マージしました。

---
+++ DVD_Rental/sasaki_masayuki/100_regression_management/regression_management.aspx.cs
@@ -0,0 +1,54 @@
+﻿using DVD_Rental.sasaki_masayuki._0_common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+namespace DVD_Rental.sasaki_masayuki._100_regression_management
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/sasaki_masayuki/101_return_confirmation/return_confirmation.aspx");
+        }
+
+
+
+        //テキストボックスに入力されているテキストを取得
+        //※テキストが入力されていない場合nullを返します。
+        private string Get_Text()
+        {
+            try
+            {
+                return TextBox1.Text;
+            }
+            catch (Exception ex)
+            {
+                C_Sasaki_Common.Catch_An_Exception(ex);
+            }
+            return null;
+        }
+
+        
+    }
+}
---
+++ DVD_Rental/sasaki_masayuki/101_return_confirmation/return_confirmation.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DVD_Rental.sasaki_masayuki._101_return_confirmation
+{
+    public partial class return_confirmation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //キャンセルボタン
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Redirect_Regression_Management();
+        }
+
+        //確定ボタン
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Redirect_Regression_Management();
+        }
+
+        //返却管理画面に遷移する関数
+        private void Redirect_Regression_Management()
+        {
+            Response.Redirect("/sasaki_masayuki/100_regression_management/regression_management.aspx");
+        }
+    }
+}
